d7877168455adfcee33621c72780090def3c638e
vagrant <vagrant@homestead>
2020-05-07 15:50:34 +0000

    Adición de la barra de búsqueda

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,10 +39,15 @@
 
         <div class="options">
           <!-- SEARCH  -->
-          <svg class="bi bi-search" width="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clip-rule="evenodd"/>
-            <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clip-rule="evenodd"/>
-          </svg>
+          <div class="search-nav">
+            <form class="" action="index.html" method="post">
+              <input type="text" name="name" placeholder="Buscar juegos o usuarios" id="search-nav">
+            </form>
+            <svg id="search-nav-icon" class="bi bi-search" width="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clip-rule="evenodd"/>
+              <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clip-rule="evenodd"/>
+            </svg>
+          </div>
           <!-- DROPDOWN ACCOUNT -->
           <div class="dropdown">
             @guest
